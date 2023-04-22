699ed1ba336b30a8787c1b5c5a6feccce9f6f6bb
CasparB <casparbroekhuizen@gmail.com>
2023-04-22 12:55:49 -0700

    GPT added

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-    apiKey: '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',
+    apiKey: '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',
 });
 const openai = new OpenAIApi(configuration);
 
@@ -11,6 +11,7 @@ exports.getGPTResponse = functions.https.onCall(async (data, context) => {
     const completion = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: text,
+        max_tokens: 1024
     });
     return completion.data;
 });

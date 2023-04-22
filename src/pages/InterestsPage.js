2ebf04455636af2e5e6cdfc8ce06ca894ed21ed0
CasparB <casparbroekhuizen@gmail.com>
2023-04-22 11:30:55 -0700

    Added interests

--- src/pages/InterestsPage.js
+++ src/pages/InterestsPage.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+import Navbar from '../components/Navbar';
+
 // Images
 import food from '../assets/food.jpeg';
 
@@ -56,33 +58,36 @@ const InterestsPage = () => {
     }
 
     return (
-        <div className='page-wrapper v-center'>
-            <div className='page-contents'>
-                <h1>Your Interests</h1>
-                <p>Help us give you better suggestions by first selecting a few categories that interest you.</p>
-                <div className='interests-grid'>
-                {interests.map((item, index) => {
-                    return (
-                        <button className={`interest-wrapper ${item.selected ? 'selected' : ''}`} 
-                            key={index}
-                            onClick={() => handleItemClick(item.type)} >
-                            <div className='interest-img-wrapper'>
-                                <img className='interest-img'
-                                    src={item.img} />
-                                <div className='mask' />
-                            </div>
-                            <div className='interest-text'>
-                                <p>{ item.type }</p>
-                            </div>
+        <div>
+            <Navbar />
+            <div className='page-wrapper v-center'>
+                <div className='page-contents'>
+                    <h1>Your Interests</h1>
+                    <p>Help us give you better suggestions by first selecting a few categories that interest you.</p>
+                    <div className='interests-grid'>
+                    {interests.map((item, index) => {
+                        return (
+                            <button className={`interest-wrapper ${item.selected ? 'selected' : ''}`} 
+                                key={index}
+                                onClick={() => handleItemClick(item.type)} >
+                                <div className='interest-img-wrapper'>
+                                    <img className='interest-img'
+                                        src={item.img} />
+                                    <div className='mask' />
+                                </div>
+                                <div className='interest-text'>
+                                    <p>{ item.type }</p>
+                                </div>
+                            </button>
+                        );
+                    })}
+                    </div>
+                    <Link to='/globe'>
+                        <button className='primary-button'>
+                            Save
                         </button>
-                    );
-                })}
+                    </Link>
                 </div>
-                <Link to='/globe'>
-                    <button className='primary-button'>
-                        Save
-                    </button>
-                </Link>
             </div>
         </div>
     )
